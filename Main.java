8cc3bdc28758088eca0787f8f40a3b4014b36e99
shreeji0312 <55413201+shreeji0312@users.noreply.github.com>
2020-07-17 14:44:23 -0400

    Main
    forgot to commit main. here is the main

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+//Temporary class to test ExcelExtractor and print the HashMap
+public class Main {
+
+    public static void main(String[]args){
+        MenuInterface test = new MenuInterface();
+        test.menuControl();
+    }
+}
